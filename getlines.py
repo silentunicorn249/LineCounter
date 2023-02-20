d58efaaf2f00ab94a7c5a861366f9327816fb407
silentunicorn249 <markbassem@hotmail.com>
2023-02-20 23:28:08 +0200

    Added to show/hide output

--- getlines.py
+++ getlines.py
@@ -13,11 +13,14 @@ def getDir(dir, extensions, directories):
 			included_ext = [y for y in map( lambda x: i.endswith("."+x), extensions)]
 			if any(included_ext):
 				# print("Done with, ", i)
-				print("Checking file: "+dir+"/"+i)
+				if show:
+					print("Checking file: "+dir+"/"+i)
 				try:
 					f = open(dir+'/'+i,'r')
 					lines = len(f.readlines())
-					print(f"file {dir+'/'+i,'r'} has {lines} lines")
+					if show: 
+						print(f"file {dir+'/'+i,'r'} has {lines} lines")
+						
 					logs.write(f"file {dir+'/'+i,'r'} has {lines} lines\n")
 					fsync(logs.fileno())
 					count+=lines
@@ -29,18 +32,25 @@ def getDir(dir, extensions, directories):
 dir = input("Enter directory: ")
 files = input("Enter file types seperated by space: ").split()
 directories = input("Enter excluded directories: ").split()
+show = input("Show output? (y/n) ")
+save = input("Save into logs? (y/n) ").lower()
+if not show or show =="y" or show == "yes":
+	show = True
+else:
+	show = False
+	
 no = 0
 while 1:
 	logname = f"logs-{no}.txt"
-	print(logname)
 	file = Path(logname)
 	if not file.is_file():
 		logs = open(logname, 'w+')
 		break
 	no+=1
-
+		
 
 count = getDir(dir, files, directories)
+
 logs.write(f"Total lines: {count}\n")
-print(f"Total lines: {count}\n")
 logs.close()
+print(f"Total lines: {count}\n")
